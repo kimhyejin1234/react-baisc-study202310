eb0809df52f26fe0504adba3a88f5d34a968fed6
“hyejin” <hyejinkim1219@naver.com>
2023-10-30 15:49:50 +0900

    1.React초기 설정 완료및 Component개념 확인

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import ExpenseItem from './components/ExpenseItem';
+import NoName from './NoName';
 function App() {
+  const $h2 = React.createElement('h2', null, '반가와요~~');
+  const $h3 = <h3>와~~~~우!!!</h3>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <NoName />
+      <NoName />
+      <NoName />
+      <div className='App'>
+        <h1>메롱 메롱</h1>
+        {$h2}
+        {$h3}
+      </div>
+      <div className='noname'>
+        <input type='text' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          오늘은 월요일 입니다. <br />
+          그래서 공부가 하기 싫어요.~~~~
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
