5edbe7f182624c04c4fd919fd8f4ba728919d54b
hj <hyejinkim1219@naver.com>
2023-10-31 14:36:08 +0900

    2023.10.31 첫번째

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
-import NoName from './NoName';
-function App() {
-  const $h2 = React.createElement('h2', null, '반가와요~~');
-  const $h3 = <h3>와~~~~우!!!</h3>;
+import { Expenses } from './components/Expenses';
+
+const App = () => {
+  //지출 항목 객체 배열
+
+  const expenses = [
+    {
+      title: '바나나',
+      price: 2000,
+      date: new Date(2023, 3 - 1, 23),
+    },
+    {
+      title: 'BBQ치킨',
+      price: 20000,
+      date: new Date(2023, 5 - 1, 21),
+    },
+    {
+      title: '도미노피자',
+      price: 35000,
+      date: new Date(2023, 7 - 1, 4),
+    },
+  ];
 
   return (
     <>
-      <NoName />
-      <NoName />
-      <NoName />
-      <div className='App'>
-        <h1>메롱 메롱</h1>
-        {$h2}
-        {$h3}
-      </div>
-      <div className='noname'>
-        <input type='text' />
-        <p>
-          오늘은 월요일 입니다. <br />
-          그래서 공부가 하기 싫어요.~~~~
-        </p>
-      </div>
+      <Expenses items={expenses} />
     </>
   );
-}
+};
 
 export default App;
